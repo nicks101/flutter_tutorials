8102a850acb52e0c27cd0f59a8eb5cb5c77156f2
Elliot Huntington <elliot.huntington@gmail.com>
2024-08-22 07:59:04 -0600

    Add test for GenericModel<MyModel>

--- built_value_generic/test/built_value_generic_test.dart
+++ built_value_generic/test/built_value_generic_test.dart
@@ -1,4 +1,29 @@
-import 'package:built_value_generic/built_value_generic.dart';
+import 'package:built_collection/built_collection.dart';
+import 'package:built_value_generic/models/generic_model.dart';
+import 'package:built_value_generic/models/my_model.dart';
 import 'package:test/test.dart';
 
-void main() {}
+void main() {
+
+  group('GenericModel<MyModel', () {
+    final GenericModel<MyModel> model = GenericModel<MyModel>(
+          (b) => b
+        ..data = ListBuilder<MyModel>([
+          MyModel((b) => b
+            ..name = 'Hello'
+            ..id = '1'),
+          MyModel((b) => b
+            ..name = 'World'
+            ..id = '2'),
+        ]),
+    );
+
+    test('deserializes serialized model', () async {
+      expect(
+        GenericModel.fromJson<MyModel>(model.toJson()),
+        model,
+      );
+    });
+  });
+
+}
